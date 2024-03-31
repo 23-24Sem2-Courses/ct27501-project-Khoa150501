7da161e1b9be51c40e7dc79a26f49e5fd16e03c7
unknown <nicktai45@gmail.com>
2024-04-01 00:40:19 +0700

    add trang gioithieu, dangnhap, dangky + chinh sua footer

---
+++ public/gioiThieu.php
@@ -0,0 +1,39 @@
+<?php
+include_once __DIR__ . '/../src/partials/header.php';
+
+?>
+
+<?php include_once __DIR__ . '/../src/partials/navbar.php'?>
+
+
+<script src="\bootstrap-5.0.2-dist\js\bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<body>
+  <div class="content">
+
+    <br>
+    <br>
+    <br>
+
+    <div class="container">
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="#">Trang chủ</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Giới thiệu</li>
+        </ol>
+      </nav>
+      <h1> Giới thiệu </h1>
+      <hr class="border border-success border-2 opacity-50">
+      <p> <strong>VinFast</strong> là công ty thành viên thuộc tập đoàn <strong>Vingroup</strong>, một trong những Tập đoàn Kinh tế tư nhân đa ngành lớn nhất Châu Á. 
+          <br><br>Với triết lý <strong>“Đặt khách hàng làm trọng tâm”</strong>, VinFast không ngừng sáng tạo để tạo ra các sản phẩm đẳng cấp và trải nghiệm xuất sắc cho mọi người.
+          <br><br><strong>VinFast</strong> đã nhanh chóng thiết lập sự hiện diện toàn cầu, thu hút những tài năng tốt nhất từ khắp nơi trên thế giới và hợp tác với một số thương hiệu mang tính biểu tượng nhất trong ngành Ô tô.
+      </p>
+    </div>
+
+    <br><br><br><br><br><br>
+    
+    
+    
+</body>
+
+<?php include_once __DIR__ . '/../src/partials/footer.php' ?>
